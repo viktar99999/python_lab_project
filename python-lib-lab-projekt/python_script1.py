e53f2500ce115cf4597fc828a18028d8d1ad068e
viktar99999 <viktar77722@gmail.com>
2022-06-30 00:27:54 +0300

    model

---
+++ python-lib-lab-projekt/python_script1.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import scipy
+import sklearn
+import matplotlib.pyplot as plt
+import seaborn as sns
+import random
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score as r2
+from sklearn.model_selection import KFold, GridSearchCV
+df = pd.read_csv('test.csv')
+df.head()
+df.shape
+df.dtypes
+df.info()
+X = df.iloc[:, 0:19]
+y = df.iloc[:, 19]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+regressor = RandomForestRegressor(n_estimators=20, random_state=42)
+regressor.fit(X_train, y_train)
+y_pred = regressor.predict(X_test)
+r2(y_test, y_pred)
+print(0.8068
+)

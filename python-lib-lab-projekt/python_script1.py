e21bbbd5472104fb174701d4de148b4ebe5cd59d
viktar99999 <viktar77722@gmail.com>
2023-09-22 10:17:30 +0300

    upgrade python_file

--- python-lib-lab-projekt/python_script1.py
+++ python-lib-lab-projekt/python_script1.py
@@ -1,18 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import random
-import numpy as np
 import pandas as pd
-import scipy
-import sklearn
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
-from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score as r2
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 df = pd.read_csv('test.csv')
 df.head()
 print(df.shape)
@@ -21,9 +13,9 @@ print(df.info())
 X = df.iloc[:, 0:19]
 y = df.iloc[:, 19]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.88, random_state=42)
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+standard = StandardScaler()
+X_train = standard.fit_transform(X_train)
+X_test = standard.transform(X_test)
 regressor = RandomForestRegressor(n_estimators=20, random_state=42)
 regressor.fit(X_train, y_train)
 y_pred = regressor.predict(X_test)
